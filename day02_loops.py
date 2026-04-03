68165a19a5d7433c9250834c1c476a773dad2e3d
ANTRISH SHARMA <antrishsharma555@gmail.com>
2026-04-03 21:59:14 +0530

    Update day02_loops.py

--- day02_loops.py
+++ day02_loops.py
@@ -7,3 +7,22 @@
 
 for i in range(1, 11):
     print(i)
+
+
+# Program: Print table of a number
+num=int(input("enter a number : "))
+for i in range(1,11):
+  print(f"{num} x {i} = {num*i}")
+
+# Program: Print numbers using while loop
+i=10
+while i>=1:
+  print(f"Number : {i}")
+  i-=1
+
+# Program: Print even numbers from 1 to 20
+for i in range(1, 21):
+    if i%2 ==0:
+        print(f"Even Number: {i}")
+
+
