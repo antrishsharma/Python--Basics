f5edf29045a1299f119b4c44538707b69dead8d0
ANTRISH SHARMA <antrishsharma555@gmail.com>
2026-04-09 23:01:00 +0530

    Add practice questions for while loops
    Added practice questions for while loops, including printing even numbers and a factorial calculation prompt.

---
+++ while-loop 01.py
@@ -0,0 +1,11 @@
+#Pratice questions on while loop
+#Q1.) WAP to print numbers from 1 to 50 using while loop
+i=1
+while i<=50:
+    if i%2==0:
+        print(f"{i}")
+        
+    i+=1    
+
+#Q2.) WAP using while print Factorial
+
